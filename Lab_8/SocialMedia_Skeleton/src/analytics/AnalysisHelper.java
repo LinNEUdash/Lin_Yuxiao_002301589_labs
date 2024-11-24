6ea501bb39ad0151c584b2bae0e9ed4adba3886d
LinNEUdash <lin.yuxia@northeastern.edu>
2024-11-24 15:45:35 -0500

    Q6-Q7

--- Lab_8/SocialMedia_Skeleton/src/analytics/AnalysisHelper.java
+++ Lab_8/SocialMedia_Skeleton/src/analytics/AnalysisHelper.java
@@ -98,75 +98,75 @@ public class AnalysisHelper {
         }
     }
     
-//    public void getPassiveCommentUsers() {
-//        
-//        DataStore data = DataStore.getInstance();
-//        
-//        HashMap<Integer, Integer> commentNumbers = new HashMap<Integer, Integer>();
-//        
-//        for (Comment c : data.getComments().values()) {
-//            
-//            int userId = c.getUserId();
-//            if (commentNumbers.containsKey(userId)) {
-//                commentNumbers.put(userId, commentNumbers.get(userId) + 1);
-//            } else {
-//                commentNumbers.put(userId, 1);
-//            }
-//        }
-//        
-//        ArrayList<User> users = new ArrayList(data.getUsers().values());
-//        
-//        Collections.sort(users, new UserMapComparator(commentNumbers));
-//        
-//        System.out.println("Q5 - The following users have the least comments: ");
-//        
-//        for (int i = 0; i < 5; i++) {
-//            System.out.println(users.get(i) + ", - Post count:" + commentNumbers.get(users.get(i).getId()));
-//        }
-//    }
-//    
-//    public void getPassiveAndActiveOverallUsers() {
-//        
-//        DataStore data = DataStore.getInstance();
-//        
-//        HashMap<Integer, Integer> overallNumbers = new HashMap<Integer, Integer>();
-//        
-//        for (Comment c : data.getComments().values()) {
-//            
-//            int userId = c.getUserId();
-//            if (overallNumbers.containsKey(userId)) {
-//                overallNumbers.put(userId, overallNumbers.get(userId) + 1 + c.getLikes());
-//            } else {
-//                overallNumbers.put(userId, 1 + c.getLikes());
-//            }
-//        }
-//        
-//        for (Post p : data.getPosts().values()) {
-//            
-//            int userId = p.getUserId();
-//            if (overallNumbers.containsKey(userId)) {
-//                overallNumbers.put(userId, overallNumbers.get(userId) + 1);
-//            } else {
-//                overallNumbers.put(userId, 1);
-//            }
-//        }
-//        
-//        ArrayList<User> users = new ArrayList(data.getUsers().values());
-//        
-//        Collections.sort(users, new UserMapComparator(overallNumbers));
-//        
-//        System.out.println("Q6 - The following users have overall been passive: ");
-//        
-//        for (int i = 0; i < 5; i++) {
-//            System.out.println(users.get(i) + ", - Overall count:" + overallNumbers.get(users.get(i).getId()));
-//        }
-//        
-//        Collections.sort(users, new UserMapComparator(overallNumbers));
-//        
-//        System.out.println("Q7 - The following users have overall been active: ");
-//        
-//        for (int i = users.size() - 1; i > users.size() - 6; i--) {
-//            System.out.println(users.get(i) + ", - Overall count:" + overallNumbers.get(users.get(i).getId()));
-//        }
-//    }
+    public void getPassiveCommentUsers() {
+        
+        DataStore data = DataStore.getInstance();
+        
+        HashMap<Integer, Integer> commentNumbers = new HashMap<Integer, Integer>();
+        
+        for (Comment c : data.getComments().values()) {
+            
+            int userId = c.getUserId();
+            if (commentNumbers.containsKey(userId)) {
+                commentNumbers.put(userId, commentNumbers.get(userId) + 1);
+            } else {
+                commentNumbers.put(userId, 1);
+            }
+        }
+        
+        ArrayList<User> users = new ArrayList(data.getUsers().values());
+        
+        Collections.sort(users, new UserMapComparator(commentNumbers));
+        
+        System.out.println("Q5 - The following users have the least comments: ");
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.println(users.get(i) + ", - Post count:" + commentNumbers.get(users.get(i).getId()));
+        }
+    }
+    
+    public void getPassiveAndActiveOverallUsers() {
+        
+        DataStore data = DataStore.getInstance();
+        
+        HashMap<Integer, Integer> overallNumbers = new HashMap<Integer, Integer>();
+        
+        for (Comment c : data.getComments().values()) {
+            
+            int userId = c.getUserId();
+            if (overallNumbers.containsKey(userId)) {
+                overallNumbers.put(userId, overallNumbers.get(userId) + 1 + c.getLikes());
+            } else {
+                overallNumbers.put(userId, 1 + c.getLikes());
+            }
+        }
+        
+        for (Post p : data.getPosts().values()) {
+            
+            int userId = p.getUserId();
+            if (overallNumbers.containsKey(userId)) {
+                overallNumbers.put(userId, overallNumbers.get(userId) + 1);
+            } else {
+                overallNumbers.put(userId, 1);
+            }
+        }
+        
+        ArrayList<User> users = new ArrayList(data.getUsers().values());
+        
+        Collections.sort(users, new UserMapComparator(overallNumbers));
+        
+        System.out.println("Q6 - The following users have overall been passive: ");
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.println(users.get(i) + ", - Overall count:" + overallNumbers.get(users.get(i).getId()));
+        }
+        
+        Collections.sort(users, new UserMapComparator(overallNumbers));
+        
+        System.out.println("Q7 - The following users have overall been active: ");
+        
+        for (int i = users.size() - 1; i > users.size() - 6; i--) {
+            System.out.println(users.get(i) + ", - Overall count:" + overallNumbers.get(users.get(i).getId()));
+        }
+    }
 }
